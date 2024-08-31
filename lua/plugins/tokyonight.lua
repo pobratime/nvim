50ae7caabc78ebad650f30f4179ec1eda5cda3da
Luka Ajdukovic <lukaajdukovicc@icloud.com>
2024-09-01 00:52:14 +0200

    Changes to be committed: 	modified:   lua/plugins/gruvbox.lua 	modified:   lua/plugins/tokyonight.lua

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,5 +2,22 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
-}
+    
+    config = function()
+
+   require("tokyonight").setup({
+  -- use the night style
+  style = "night",
+  -- disable italic for functions
+  styles = {
+    functions = {}
+  },
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  on_colors = function(colors)
+    colors.hint = colors.orange
+    colors.error = "#ff0000"
+  end
+})
+
+    end
+  }
